24c324a4370bf60cae0291473c8baaed99a43eb1
Antoine Belvire <antoine.belvire@laposte.net>
2015-11-11 15:41:05 +0100

    Add send function

--- Makefile
+++ Makefile
@@ -12,14 +12,19 @@ directories:
 	cd $(BASEDIR)
 	mkdir -p $(BUILDDIR)
 
-tests: $(SRCDIR)/at-commands.o $(TESTDIR)/test-at-commands.o 
-	$(CC) $(CFLAGS) $(TESTDIR)/test-at-commands.o $(SRCDIR)/at-commands.o -o $(BUILDDIR)/test-leonard
+tests: $(SRCDIR)/at-commands.o $(SRCDIR)/sender.o $(TESTDIR)/test-at-commands.o $(TESTDIR)/test-sender.o
+	$(CC) $(CFLAGS) $(TESTDIR)/test-at-commands.o $(SRCDIR)/at-commands.o -o $(BUILDDIR)/test-at-commands
+	$(CC) $(CFLAGS) $(TESTDIR)/test-sender.o $(SRCDIR)/sender.o -o $(BUILDDIR)/test-sender
 
 at-commands.o:
 	$(CC) $(CFLAGS) -c at-commands.c
 
 test-at-commands.o: $(SRCDIR)/at-commands.c $(TESTDIR)/test-at-commands.c
 	$(CC) $(CFLAGS) -c at-commands.c test-at-commands.c
+
+test-sender.o: $(SRCDIR)/sender.c $(TESTDIR)/test-sender.c
+	$(CC) $(CFLAGS) -c sender.c test-sender.c
+
 
 clean:
 	rm $(BUILDDIR)/*
---
+++ src/sender.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sender.h"
+
+#define MIN(a,b) (((a)<(b))?(a):(b))
+
+static struct sockaddr_in serv_addr;
+static int socket_id;
+
+int send_message(char *message)
+{
+    if (sendto(socket_id, message, MIN(strlen(message), MAX_BUF_LEN), 0, (struct sockaddr *)&serv_addr, (socklen_t) sizeof(serv_addr)) == -1)
+    {
+        fprintf(stderr, "[%s:%d] Error: sendto() failed\n", __FILE__, __LINE__);
+        return 1;
+    }
+
+    close(socket_id);
+    return 0;
+}
+
+int initialize_socket()
+{
+    if ((socket_id = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
+    {
+        fprintf(stderr, "[%s:%d] Error: Socket creation failed\n", __FILE__, __LINE__);
+	return 1;
+    }
+
+    // Addresses
+    bzero(&serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(DEST_PORT);
+
+    if (inet_aton(DEST_IP, &serv_addr.sin_addr) == 0)
+    {
+        fprintf(stderr, "[%s:%d] Error: inet_aton() failed\n", __FILE__, __LINE__);
+        return 2;
+    }
+
+    return 0;
+}
