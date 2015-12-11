4848d72a003dbd756cef856def59bcaa796a692b
Lucille SAADE <lucillesaade@gmail.com>
2015-12-11 15:11:51 +0100

    Update Localisation and reception

--- src/uart/main.c
+++ src/uart/main.c
@@ -3,13 +3,14 @@
 #include <string.h>  /* String function definitions */
 #include "parser.h"
 #include "parameters.h"
+#include "localisation.h"
 
 
 int main() 
 {
 	t_data s;
 	
-	s = extract_data("4609ad800700 6\r\n");
+	s = extract_data("46 09 ad 80 07 00 -6\r\n");
 	
 	printf("rssi beacon 1 : %d\n", getBeaconRssi(0)); 
 	return 0;
