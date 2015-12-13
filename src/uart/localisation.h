f13745ce6513991d61cb7d6b0fe50aeb685c9ca3
Lucille SAADE <lucillesaade@gmail.com>
2015-12-13 18:55:56 +0100

    Update Localisation

--- src/uart/localisation.h
+++ src/uart/localisation.h
@@ -1,14 +1,19 @@
 #ifndef LOCALISATION_H
 #define LOCALISATION_H
 
+#include <stdio.h>   
+#include <stdlib.h>
+#include "parameters.h"
 
-void updateCurrentLocation();
+
+void UpdateCurrentLocation();
 
 // return the closest beacon different from beacon in1 and beacon in2 if in1, in2!=-1
 uint8_t getIndexOfCloserBeacon(int8_t ind1, int8_t ind2);
 
 // GETTER
 t_location getCurrentLocation(void);
+
 
 #endif
 
