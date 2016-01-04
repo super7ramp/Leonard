fc09a92d9aa25912744b89d225127b4db6533dd0
Lucille SAADE <lucillesaade@gmail.com>
2016-01-04 15:24:15 +0100

    update pcCom

--- src/com/sender.h
+++ src/com/sender.h
@@ -5,7 +5,7 @@ void construire_message(char *message, char *contenu);
 void afficher_messageS(char *message, int lg);
 void afficher_envoi (int lg_message, char * message);
 int initSender(char * nom_station);
-int emettre(int lg_message, char * message,char *contenu);
+int emettre(int lg_message, char * message, char *contenu);
 int closeSender();
 
 #endif
