cdf4dcf1d5a0412b055e120b60c2e26e9ce14b69
Alexis Lothoré <alexis.lothore@gmail.com>
2016-01-07 11:38:44 +0100

    Added some debug print for bluetooth localisation

--- src/uart/display.c
+++ src/uart/display.c
@@ -25,4 +25,11 @@ void printPositionOnMap(t_location pos)
   printTab();
   printf("\033[%dA", (int)H+6);
 }
+
+void printBasicInformation(t_location pos)
+{
+  printf("Visible beacons : ");
+  printVisibleBeacons();
+  printf("X : %.2f - Y : %2.f\r\n", pos.x, pos.y);
+}
 	    
