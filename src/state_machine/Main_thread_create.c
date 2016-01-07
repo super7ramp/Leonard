17b91cdce18a76a0dfd0564c82ac81872a4d6f73
Lucille SAADE <lucillesaade@gmail.com>
2016-01-07 11:50:52 +0100

    Update script state machine

--- src/state_machine/Main_thread_create.c
+++ src/state_machine/Main_thread_create.c
@@ -23,11 +23,11 @@ int main(int argc, char *argv[])
     perror("pthread_create");
     return 1;
   }
-  if(pthread_create(&threadControl, NULL, controlTask, NULL) == -1)
+ /* if(pthread_create(&threadControl, NULL, controlTask, NULL) == -1)
   {
     perror("pthread_create");
     return 1;
-  }
+  }*/
   if(pthread_create(&threadBluetooth, NULL, thread_Bluetooth, NULL) == -1)
   {
     perror("pthread_create");
@@ -38,11 +38,11 @@ int main(int argc, char *argv[])
     perror("pthread_join");
     return 1;
   }
-  if (pthread_join(threadControl, NULL)) 
+ /* if (pthread_join(threadControl, NULL)) 
   {
     perror("pthread_join");
     return 1;
-  }
+  }*/
   if (pthread_join(threadBluetooth, NULL)) 
   {
     perror("pthread_join");
