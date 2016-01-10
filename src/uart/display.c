f7b2bd57772ff525e1e9af7354ce2701c6780cb3
Alexis Lothoré <alexis.lothore@gmail.com>
2016-01-10 12:09:07 +0100

    Separated some debug output in proper functions. Modified location parameters to improve stability

--- src/uart/display.c
+++ src/uart/display.c
@@ -1,29 +1,34 @@
 #include "display.h"
 #include "parser.h"
 
+void printLocationFullDebug(t_location pos)
+{
+  printPositionOnMap(pos);
+  printf("Visible beacons : ");
+  printVisibleBeacons();
+  printf("X : %.2f - Y : %.2f\r\n", pos.x, pos.y);
+  printTab();
+  printf("\033[%dA", (int)(H*ZOOM)+6);
+}
+
 void printPositionOnMap(t_location pos)
 {
   int i,j;
-  for (j=0; j<H; j++)
+  for (j=0; j<H*ZOOM; j++)
     {
-      for(i=0; i<W; i++)
+      for(i=0; i<W*ZOOM; i++)
 	{
-	  if((int)(pos.x)==i && (int)(pos.y)==(int)(H)-j)
+	  if((int)(pos.x*ZOOM)==i && (int)(pos.y*ZOOM)==(int)(H*ZOOM)-j)
 	    printf(" x ");
-	  else if(j==0||j==(int)(H)-1)
+	  else if(j==0||j==(int)(H*ZOOM)-1)
 	    printf("==");
-	  else if(i==0||i==(int)(W)-1)
+	  else if(i==0||i==(int)(W*ZOOM)-1)
 	    printf("|");
 	  else
 	    printf("   ");
 	}
       printf("\r\n");
     }
-  printf("Visible beacons : ");
-  printVisibleBeacons();
-  printf("X : %.2f - Y : %2.f\r\n", pos.x, pos.y);
-  printTab();
-  printf("\033[%dA", (int)H+6);
 }
 
 void printBasicInformation(t_location pos)
--- src/uart/display.h
+++ src/uart/display.h
@@ -2,6 +2,9 @@
 #include "stdio.h"
 #include "stdlib.h"
 
+#define ZOOM	2	
+
+void printLocationFullDebug(t_location pos);
 void printPositionOnMap(t_location pos);
 void printBasicInformation(t_location pos);
 
