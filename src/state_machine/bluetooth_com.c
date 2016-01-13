01cf3b465048f3a5cf650f3f8ea11ffabaebd48a
Alexis Lothoré <alexis.lothore@gmail.com>
2016-01-13 16:41:17 +0100

    [State Machine] Replaced sprint review demo with fictive data by the real thread (no .c extension to allow use of Makefile without compiling the demo)

--- src/state_machine/bluetooth_com.c
+++ src/state_machine/bluetooth_com.c
@@ -1,41 +1,19 @@
 #include "bluetooth_com.h"
 
-typedef struct
-{
-    float x;
-    float y;
-} coord_t;
-
 void *thread_Bluetooth(void *arg)
 {
-    int i;
-    for(i = 0; i < 5; i++)
-    {
-        // Wait for user input
-        getc(stdin);
-	computeLocation();
-    }
+    initLocationComputation();
+
+	while(1)
+	{
+		// Blocking function, cannot go faster than 500/8 ms
+		computeLocation();
+	}
 }
 
 void read_data_bluetooth(float* _x, float* _y)
-{    
-    coord_t positions[5];
-
-    positions[4].x = 3.25; positions[4].y = 11.0; // destination
-    positions[3].x = 2.17; positions[3].y = 9.9;
-    positions[2].x = 2.17; positions[2].y = 8.8;
-    positions[1].x = 3.25; positions[1].y = 7.7;
-    positions[0].x = 4.33; positions[0].y = 6.6; // start point
-
-    static int i = 0;
-    if(i >= 0 && i < 5)
-    {
-        *_x = positions[i].x;
-        *_y = positions[i].y;
-    }
-    else
-    {
-        *_x = positions[4].x;
-        *_y = positions[4].y;
-    }
+{
+	t_location pos=getCurrentLocation();
+    *_x = pos.x;
+	*_y = pos.y;
 }
